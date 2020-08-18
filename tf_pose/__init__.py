01dd04da126a61aa3052fde4f259d252388b5a31
nhatdt51 <57204739+nhatdt51@users.noreply.github.com>
2020-08-18 14:10:36 +0800

    Update __init__.py

--- tf_pose/__init__.py
+++ tf_pose/__init__.py
@@ -1,7 +1,6 @@
-__version__ = '0.1.1'
-__git_version__ = __version__
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
 from tf_pose.runner import infer, Estimator, get_estimator
+
+__version__ = '0.1.1'
